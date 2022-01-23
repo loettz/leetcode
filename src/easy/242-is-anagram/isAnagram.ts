404e3ed3631a06c2c0d4067b4eb6af593f0ee3f3
Lotte Hansen <lotte_hansen@web.de>
2022-01-23 12:06:12 +0100

    use arrow functions

--- src/easy/242-is-anagram/isAnagram.ts
+++ src/easy/242-is-anagram/isAnagram.ts
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/valid-anagram/
 
-export function isAnagram(string1: string, string2: string): boolean {
+export const isAnagram = (string1: string, string2: string): boolean => {
 
     const sourceArray = string1.split('').sort();
     const targetArray = string2.split('').sort();
--- src/easy/58-length-of-last-word/getLengthOfLastWord.ts
+++ src/easy/58-length-of-last-word/getLengthOfLastWord.ts
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/length-of-last-word/
 
-export function getLengthOfLastWord(s: string): number {
+export const getLengthOfLastWord = (s: string): number => {
     let stringArr = s.split(' ');
     stringArr = stringArr.filter((string) => string !== "")
     return stringArr[stringArr.length-1].length
