df67a1d7a2c12274202791983f325a83346d44f4
Lotte Hansen <lotte_hansen@web.de>
2022-01-23 12:04:22 +0100

    update contains duplicate

--- src/easy/217-contains-duplicate/containsDuplicate.ts
+++ src/easy/217-contains-duplicate/containsDuplicate.ts
@@ -1,7 +1,5 @@
 //https://leetcode.com/problems/contains-duplicate/
 
 export const containsDuplicate = (numbers: number[]): boolean => {
-    return numbers.some((number:number) => {
-        return numbers.indexOf(number) !== numbers.lastIndexOf(number)
-    })
+    return (new Set(numbers)).size !== numbers.length;
 }
